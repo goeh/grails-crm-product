f0b34aa63e602de0f89e8095685446f3a38d7028
Goran Ehrsson <goran@technipelago.se>
2013-03-16 19:55:48 +0100

    Added cache configuration

---
+++ grails-app/conf/CrmProductCacheConfig.groovy
@@ -0,0 +1,38 @@
+import grails.plugins.crm.product.CrmPriceList
+import grails.plugins.crm.product.CrmProduct
+import grails.plugins.crm.product.CrmProductGroup
+
+grails.cache.config = {
+// Hibernate domain class second-level caches.
+    domain {
+        name CrmProduct
+        maxElementsInMemory 1000
+        timeToLiveSeconds 300
+        timeToIdleSeconds 300
+
+        collection {
+            name 'prices'
+            eternal false
+            overflowToDisk false
+            maxElementsInMemory 1000
+            maxElementsOnDisk 0
+            timeToLiveSeconds 300
+            timeToIdleSeconds 300
+        }
+    }
+
+    domain {
+        name CrmProductGroup
+        maxElementsInMemory 100
+        timeToLiveSeconds 300
+        timeToIdleSeconds 300
+    }
+
+
+    domain {
+        name CrmPriceList
+        maxElementsInMemory 100
+        timeToLiveSeconds 300
+        timeToIdleSeconds 300
+    }
+}
