dd30d1c8261e7dfb78ad98f19fe28ac7e0ebe5e1
Goran Ehrsson <goran@technipelago.se>
2013-11-30 19:21:21 +0100

    Version 1.2.1 (moved feature declaration from crm-product-ui)

--- CrmProductGrailsPlugin.groovy
+++ CrmProductGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class CrmProductGrailsPlugin {
     def groupId = "grails.crm"
-    def version = "1.2.0"
+    def version = "1.2.1"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmContact']
@@ -9,16 +9,29 @@ class CrmProductGrailsPlugin {
             "src/groovy/grails/plugins/crm/product/TestSecurityDelegate.groovy",
             "grails-app/views/error.gsp"
     ]
-    def title = "Grails CRM Product Plugin"
+    def title = "GR8 CRM Product Plugin"
     def author = "Goran Ehrsson"
     def authorEmail = "goran@technipelago.se"
     def description = '''\
-Provides product/item management for orders and sales projects, etc.
+A GR8 CRM plugin that provides product/item management for orders and sales projects, etc.
 '''
-    def documentation = "http://grails.org/plugin/crm-product"
+    def documentation = "https://github.com/goeh/grails-crm-product"
     def license = "APACHE"
     def organization = [name: "Technipelago AB", url: "http://www.technipelago.se/"]
     def issueManagement = [system: "github", url: "https://github.com/goeh/grails-crm-product/issues"]
     def scm = [url: "https://github.com/goeh/grails-crm-product"]
 
+    def features = {
+        crmProduct {
+            description "Product Catalogue"
+            link controller: "crmProduct", action: "index"
+            permissions {
+                guest "crmProduct:index,list,show"
+                partner "crmProduct:index,list,show"
+                user "crmProduct:*"
+                admin "crmProduct,crmProductGroup,crmPriceList:*"
+            }
+            hidden true
+        }
+    }
 }
